315e3f6a7b3ea466d80e4de8e471a885d9042e37
polarisid <danielcarvalhorst@gmail.com>
2022-06-15 12:12:58 -0300

    feat- Get trending hashtags /hashtags

---
+++ src/repositories/hashtagRepository.js
@@ -0,0 +1,34 @@
+import db from "./../config/db.js";
+
+async function insertHashtag(hashtags, postId) {
+	const querySrtring = `
+    with hashtag as (
+        insert into hashtags (name)
+        values ($1)
+        returning id
+    )
+    insert into "postsHashtags" ("idPost", "idHashtag")
+    values ($2, (select id from hashtag));
+`;
+	let array = [];
+	for (let i = 0; i < hashtags.length; i++) {
+		let Args = [hashtags[i], postId];
+		array.push(await db.query(querySrtring, Args));
+	}
+	return array;
+}
+
+async function getHashtags() {
+	return db.query(`
+    SELECT hashtags.id,hashtags.name FROM "postsHashtags" AS ph
+    JOIN hashtags ON ph."idHashtag"=hashtags.id
+    GROUP BY hashtags.name,ph."idHashtag",hashtags.id
+    ORDER BY COUNT (ph."idHashtag") DESC 
+    LIMIT 10;`);
+}
+
+const hashtagRepository = {
+	insertHashtag,
+	getHashtags,
+};
+export default hashtagRepository;
