02892615ba98ddce29c61c208776312e788cf4af
Laerte <laerteakira@gmail.com>
2022-06-17 10:43:42 -0300

    feat: Middleware to validate existance of link

---
+++ src/middlewares/linkValidation.js
@@ -0,0 +1,14 @@
+import urlExist from 'url-exist';
+
+export default async function linkValidation(req, res, next){
+    const { url } = req.body;
+    try {
+        const existUrl = await urlExist(url);
+        if(!existUrl) return res.status(404).send('Não é um Link válido!');
+
+        next();
+    } catch (error) {
+        res.sendStatus(404);
+        console.log(error.message);
+    }
+}
