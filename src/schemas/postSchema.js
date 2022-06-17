c7632ff32e6f8234f76703b7f5e1e6783545caad
Laerte <laerteakira@gmail.com>
2022-06-17 09:29:29 -0300

    fix: Validation of Post

--- src/schemas/postSchema.js
+++ src/schemas/postSchema.js
@@ -1,7 +1,9 @@
 import joi from 'joi';
 
+const pattern = '/^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm';
+
 const postSchema = joi.object({
-    url: joi.string().uri().required(),
+    url: joi.string().regex(pattern).required(),
     description: joi.string()
 });
 
