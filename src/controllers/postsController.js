2cac281a1b0be283b904c00358ca4c28f777f841
shermondevelopment <victorshermon@yahoo.com>
2022-06-19 10:55:08 -0300

    deletando relacionamento entre post e hashtag

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -80,6 +80,8 @@ export async function deletePost (req, res) {
           return res.sendStatus(401)
         }
 
+        await hastagRepository.deletePostHashTags(postId)
+
         await postRepository.deletePost(postId)
 
         res.sendStatus(204)
