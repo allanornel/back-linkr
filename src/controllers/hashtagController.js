bb9db2aa0dcdfaf3eab212a6181dcbe1ad6cb966
allanornel <allanornel@gmail.com>
2022-06-20 14:53:07 -0300

    fix: getHashtagByName

--- src/controllers/hashtagController.js
+++ src/controllers/hashtagController.js
@@ -17,6 +17,14 @@ export async function getHashtagByName(req, res) {
     const checkHashtag = await hashtagRepository.checkHashtagByName(hashtag);
     if (checkHashtag.rowCount === 0) return res.sendStatus(404);
     const posts = await hashtagRepository.getHashtagByName(hashtag);
+    await Promise.all(
+      posts.rows.map(async (post) => {
+        const { title, image, description } = await urlMetadata(post.url);
+        post.title = title;
+        post.postImage = image;
+        post.urlDescription = description;
+      })
+    );
     res.send(posts.rows);
   } catch (error) {
     console.log(error);
