6a4dbd674e05eb00cfc12190b57a9ddb9829919f
Laerte <laerteakira@gmail.com>
2022-06-21 18:32:14 -0300

    feat: search Comments from post

--- src/repositories/commentRepository.js
+++ src/repositories/commentRepository.js
@@ -7,8 +7,18 @@ async function createComment(comment, postId, userId){
     `, [comment, postId, userId]);
 }
 
+async function searchComments(postId){
+    return db.query(`
+        SELECT comments."userId", comments.comment, users.username, users.picture
+        FROM comments
+        JOIN users ON comments."userId" = users.id
+        WHERE comments."postId" = 154;
+    `, postId);
+}
+
 const commentRepository = {
-    createComment
+    createComment,
+    searchComments
 }
 
 export default commentRepository;
