60c2b2fcc3ab1dd8e2488e2199a9729a0da0ab96
shermondevelopment <victorshermon@yahoo.com>
2022-06-21 21:43:56 -0300

    adicionando feature de seguir e deixa de seguir usuarios

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,6 +3,7 @@ import authRouter from "./authRouter.js";
 import hashtagsRouter from "./hashtagsRouter.js";
 import postsRouter from "./postsRouter.js";
 import likesRouter from "./likesRouter.js";
+import followerRouter from "./followerRouter.js"
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use(authRouter);
 router.use(hashtagsRouter);
 router.use(postsRouter);
 router.use(likesRouter);
+router.use(followerRouter);
 
 export default router;
